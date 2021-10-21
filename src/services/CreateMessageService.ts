480ef97983a37df9822e88ee360b038ca17ead98
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2021-10-20 21:37:11 -0300

    CreateMessage service

---
+++ src/services/CreateMessageService.ts
@@ -0,0 +1,36 @@
+import { getCustomRepository } from "typeorm";
+import { io } from "../app";
+
+import { MessagesRepositories } from "../repositories/MessagesRepositories";
+
+export class CreateMessageService {
+    static async execute(text: string, user_id: string) {
+        const messagesRepository = getCustomRepository(MessagesRepositories);
+
+        const message = messagesRepository.create({
+            text,
+            user_id,
+        });
+
+        await messagesRepository.save(message, {});
+
+        const messagesWithDateUser = await messagesRepository.findOne({
+            where: { id: message.id },
+            relations: ["userId"],
+        });
+
+        const infoWS = {
+            text: messagesWithDateUser.text,
+            user_id: messagesWithDateUser.user_id,
+            created_at: messagesWithDateUser.created_at,
+            userId: {
+                name: messagesWithDateUser.userId.name,
+                avatar_url: messagesWithDateUser.userId.avatar_url,
+            },
+        };
+
+        io.emit("new_message", infoWS);
+
+        return messagesWithDateUser;
+    }
+}
