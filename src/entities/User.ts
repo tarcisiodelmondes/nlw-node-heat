61ffdae05d98c1c90a6a4b5ec2c86236db8d18c7
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2021-10-20 21:34:57 -0300

    User entity

---
+++ src/entities/User.ts
@@ -0,0 +1,30 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("users")
+export class User {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  login: string;
+
+  @Column()
+  avatar_url: string;
+
+  @Column()
+  github_id: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) this.id = uuid();
+  }
+}
